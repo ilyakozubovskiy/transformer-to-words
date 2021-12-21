9df2c0907b49b8adc4675e47da25c77449035aba
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2021-12-21 15:21:39 +0300

    Transformer in words

--- TransformerToWords/Transformer.cs
+++ TransformerToWords/Transformer.cs
@@ -1,4 +1,8 @@
-using System;
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+#pragma warning disable CA1822
 
 namespace TransformerToWords
 {
@@ -19,7 +23,105 @@ namespace TransformerToWords
         /// </example>
         public string[] Transform(double[] source)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(source)} cannot be empty.");
+            }
+
+            string[] result = new string[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                result[i] = this.TransformToWords(source[i]);
+            }
+
+            return result;
+        }
+
+        public string TransformToWords(double number)
+        {
+            if (double.IsNaN(number))
+            {
+                return "Not a Number";
+            }
+
+            if (double.IsNegativeInfinity(number))
+            {
+                return "Negative Infinity";
+            }
+
+            if (double.IsPositiveInfinity(number))
+            {
+                return "Positive Infinity";
+            }
+
+            if (number == double.Epsilon)
+            {
+                return "Double Epsilon";
+            }
+
+            StringBuilder res = new StringBuilder();
+            var stringNumber = number.ToString(CultureInfo.InvariantCulture);
+
+            if (double.IsNegative(number))
+            {
+                res.Append("Minus");
+            }
+
+            for (int i = 0; i < stringNumber.Length; i++)
+            {
+                switch (stringNumber[i] - '0')
+                {
+                    case 0:
+                        res.Append(" zero");
+                        break;
+                    case 1:
+                        res.Append(" one");
+                        break;
+                    case 2:
+                        res.Append(" two");
+                        break;
+                    case 3:
+                        res.Append(" three");
+                        break;
+                    case 4:
+                        res.Append(" four");
+                        break;
+                    case 5:
+                        res.Append(" five");
+                        break;
+                    case 6:
+                        res.Append(" six");
+                        break;
+                    case 7:
+                        res.Append(" seven");
+                        break;
+                    case 8:
+                        res.Append(" eight");
+                        break;
+                    case 9:
+                        res.Append(" nine");
+                        break;
+                    case -2:
+                        res.Append(" point");
+                        break;
+                    case -5:
+                        res.Append(" plus");
+                        break;
+                    case 21:
+                        res.Append(" E");
+                        break;
+                }
+            }
+
+            char[] result = res.ToString().Trim().ToCharArray();
+            result[0] = char.ToUpper(result[0], CultureInfo.InvariantCulture);
+
+            return new string(result);
         }
     }
 }
